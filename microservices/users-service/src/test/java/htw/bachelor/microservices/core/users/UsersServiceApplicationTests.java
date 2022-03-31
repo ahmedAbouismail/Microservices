861eb5b284c1ccbe172c179d81d442c967c529ae
ahmedAbouismail <ahmed.abouismail96@gmail.com>
2022-03-31 20:52:59 +0200

    Add Users-Service

---
+++ microservices/users-service/src/test/java/htw/bachelor/microservices/core/users/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.bachelor.microservices.core.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
