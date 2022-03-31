3ea8c603dc4eb8fcd90e126d1308edb89ebcda44
ahmedAbouismail <ahmed.abouismail96@gmail.com>
2022-03-31 20:36:21 +0200

    Add Chat-Composite-Server

---
+++ microservices/chat-composite-service/src/main/java/htw/bachelor/microservices/core/chatcomposite/ChatCompositeServiceApplication.java
@@ -0,0 +1,13 @@
+package htw.bachelor.microservices.core.chatcomposite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatCompositeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatCompositeServiceApplication.class, args);
+    }
+
+}
---
+++ microservices/chat-composite-service/src/test/java/htw/bachelor/microservices/core/chatcomposite/ChatCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.bachelor.microservices.core.chatcomposite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatCompositeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
