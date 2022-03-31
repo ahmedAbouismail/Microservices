841aa68766c096bcc2665f341aa31882125ba9a9
ahmedAbouismail <ahmed.abouismail96@gmail.com>
2022-03-31 21:06:30 +0200

    Add Status-Service

---
+++ microservices/status-service/src/main/java/htw/bachelor/microservices/core/status/StatusServiceApplication.java
@@ -0,0 +1,13 @@
+package htw.bachelor.microservices.core.status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatusServiceApplication.class, args);
+    }
+
+}
---
+++ microservices/status-service/src/test/java/htw/bachelor/microservices/core/status/StatusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.bachelor.microservices.core.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
