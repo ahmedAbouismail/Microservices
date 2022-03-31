7fe004e9f6ed500cea67994c48dd04ed66747996
ahmedAbouismail <ahmed.abouismail96@gmail.com>
2022-03-31 20:59:49 +0200

    Add Message-Service

---
+++ microservices/message-service/src/test/java/htw/bachelor/microservices/core/message/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.bachelor.microservices.core.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
